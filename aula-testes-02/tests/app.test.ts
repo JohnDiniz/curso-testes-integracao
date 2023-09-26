fc061689f318db997491ee2ee30f3f2518a13f3b
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-09-26 14:14:48 -0300

    feat: :white_check_mark: Aula04

---
+++ aula-testes-02/tests/app.test.ts
@@ -0,0 +1,11 @@
+import app from "../src/app";
+import supertest from "supertest";
+
+describe("GET /health", () => {
+  it("deve retornar status 200 ao acessar a rota /health", async () => {
+    const result = await supertest(app).get("/health");
+    const status = result.status;
+
+    expect(status).toEqual(200);
+  });
+});
